1690e7e2406e72c42fcb7de821f044bc491652bf
Sarthak Jain <sajain314@gmail.com>
2024-06-25 01:14:08 -0700

    Creating Residual Image Encoder

---
+++ env_pred/models/image_encoder.py
@@ -0,0 +1,85 @@
+import torch
+import torch.nn as nn
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1):
+        super(ResidualBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_channels != out_channels:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(out_channels)
+            )
+
+    def forward(self, x):
+        out = self.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = self.relu(out)
+        return out
+
+class ResidualEncoder(nn.Module):
+    def __init__(self):
+        super(ResidualEncoder, self).__init__()
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        self.layer1 = self._make_layer(64, 64, 2)
+        self.layer2 = self._make_layer(64, 128, 2, stride=2)
+        self.layer3 = self._make_layer(128, 256, 2, stride=2)
+        self.layer4 = self._make_layer(256, 512, 2, stride=2)
+
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512, 2048)
+
+    def _make_layer(self, in_channels, out_channels, num_blocks, stride=1):
+        layers = []
+        layers.append(ResidualBlock(in_channels, out_channels, stride))
+        for _ in range(1, num_blocks):
+            layers.append(ResidualBlock(out_channels, out_channels))
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        intermediate_outputs = []
+
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        intermediate_outputs.append(x)
+
+        x = self.layer1(x)
+        intermediate_outputs.append(x)
+
+        x = self.layer2(x)
+        intermediate_outputs.append(x)
+
+        x = self.layer3(x)
+        intermediate_outputs.append(x)
+
+        x = self.layer4(x)
+        intermediate_outputs.append(x)
+
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.fc(x)
+
+        return x, intermediate_outputs
+
+# Test the encoder
+encoder = ResidualEncoder()
+input_tensor = torch.randn(1, 3, 256, 256)  # Batch size 1, 3 channels, 256x256 image
+output, intermediate_outputs = encoder(input_tensor)
+
+print("Output shape:", output.shape)
+print("Number of intermediate outputs:", len(intermediate_outputs))
+for i, inter_output in enumerate(intermediate_outputs):
+    print(f"Intermediate output {i+1} shape:", inter_output.shape)
