daf3cd2a33dd407babd5762a26fb5369b1477849
Sarthak Jain <sajain314@gmail.com>
2024-07-09 04:21:48 -0700

    Updating Pix2PixHD model

--- env_pred/models/Pix2PixHD.py
+++ env_pred/models/Pix2PixHD.py
@@ -3,35 +3,62 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torchvision import models
 
-class GlobalGenerator(nn.Module):
-    def __init__(self, input_nc, output_nc, ngf=64):
-        super(GlobalGenerator, self).__init__()
-        # Simplified global generator
-        self.main = nn.Sequential(
-            nn.Conv2d(input_nc, ngf, 7, padding=3),
-            nn.InstanceNorm2d(ngf),
-            nn.ReLU(True),
-            nn.Conv2d(ngf, 2*ngf, 3, stride=2, padding=1),
-            nn.InstanceNorm2d(2*ngf),
-            nn.ReLU(True),
-            nn.Conv2d(2*ngf, 4*ngf, 3, stride=2, padding=1),
-            nn.InstanceNorm2d(4*ngf),
-            nn.ReLU(True),
-            nn.ConvTranspose2d(4*ngf, 2*ngf, 3, stride=2, padding=1, output_padding=1),
-            nn.InstanceNorm2d(2*ngf),
-            nn.ReLU(True),
-            nn.ConvTranspose2d(2*ngf, ngf, 3, stride=2, padding=1, output_padding=1),
-            nn.InstanceNorm2d(ngf),
-            nn.ReLU(True),
-            nn.Conv2d(ngf, output_nc, 7, padding=3),
-            nn.Tanh()
+class ResidualBlock(nn.Module):
+    def __init__(self, in_features):
+        super(ResidualBlock, self).__init__()
+        self.conv_block = nn.Sequential(
+            nn.Conv2d(in_features, in_features, 3, padding=1),
+            nn.InstanceNorm2d(in_features),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_features, in_features, 3, padding=1),
+            nn.InstanceNorm2d(in_features)
         )
 
     def forward(self, x):
-        return self.main(x)
+        return x + self.conv_block(x)
+
+class GlobalGenerator(nn.Module):
+    def __init__(self, input_nc, output_nc, ngf=32, n_downsampling=3, n_blocks=6):
+        super(GlobalGenerator, self).__init__()
+        self.input_nc = input_nc
+        self.output_nc = output_nc
+        self.ngf = ngf
+        
+        model = [nn.ReflectionPad2d(3),
+                 nn.Conv2d(input_nc, ngf, kernel_size=7, padding=0),
+                 nn.InstanceNorm2d(ngf),
+                 nn.ReLU(True)]
+
+        # Downsampling
+        for i in range(n_downsampling):
+            mult = 2**i
+            model += [nn.Conv2d(ngf * mult, ngf * mult * 2, kernel_size=3, stride=2, padding=1),
+                      nn.InstanceNorm2d(ngf * mult * 2),
+                      nn.ReLU(True)]
+
+        # Residual blocks
+        mult = 2**n_downsampling
+        for i in range(n_blocks):
+            model += [ResidualBlock(ngf * mult)]
+
+        # Upsampling
+        for i in range(n_downsampling):
+            mult = 2**(n_downsampling - i)
+            model += [nn.ConvTranspose2d(ngf * mult, int(ngf * mult / 2), kernel_size=3, stride=2, padding=1, output_padding=1),
+                      nn.InstanceNorm2d(int(ngf * mult / 2)),
+                      nn.ReLU(True)]
+
+        model += [nn.ReflectionPad2d(3),
+                  nn.Conv2d(ngf, output_nc, kernel_size=7, padding=0),
+                  nn.Tanh()]
+
+        self.model = nn.Sequential(*model)
+
+    def forward(self, input):
+        return self.model(input)
 
 class MultiscaleDiscriminator(nn.Module):
-    def __init__(self, input_nc, ndf=64, n_layers=3, num_D=3):
+    def __init__(self, input_nc, ndf=32, n_layers=3, num_D=2):
         super(MultiscaleDiscriminator, self).__init__()
         self.num_D = num_D
         self.n_layers = n_layers
@@ -57,7 +84,7 @@ class MultiscaleDiscriminator(nn.Module):
         outputs = []
         for model in self.models:
             outputs.append(model(x))
-            x = nn.functional.avg_pool2d(x, kernel_size=3, stride=2, padding=1, count_include_pad=False)
+            x = F.avg_pool2d(x, kernel_size=3, stride=2, padding=1, count_include_pad=False)
         return outputs
 
 class VGG19(nn.Module):
@@ -89,3 +116,23 @@ class VGG19(nn.Module):
         h_relu4 = self.slice4(h_relu3)
         h_relu5 = self.slice5(h_relu4)
         return [h_relu1, h_relu2, h_relu3, h_relu4, h_relu5]
+
+# Full Pix2PixHD model
+class Pix2PixHD(nn.Module):
+    def __init__(self, input_nc, output_nc, ngf=32, ndf=32, n_downsample_global=3, n_blocks_global=6,
+                 n_layers_D=3, num_D=2):
+        super(Pix2PixHD, self).__init__()
+        self.generator = GlobalGenerator(input_nc, output_nc, ngf, n_downsample_global, n_blocks_global)
+        self.discriminator = MultiscaleDiscriminator(input_nc + output_nc, ndf, n_layers_D, num_D)
+        self.vgg = VGG19()
+
+    def forward(self, input):
+        return self.generator(input)
+
+if __name__ == '__main__':
+    model = Pix2PixHD(3, 3)
+    print('Number of generator parameters:', sum(p.numel() for p in model.parameters() if p.requires_grad))
+    x = torch.randn((5, 3, 256, 256))
+    print("Input shape:", x.shape)
+    pred = model(x)
+    print("Output shape:", pred.shape)
